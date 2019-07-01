0ff51591fe3e30e998b00472a9cc821a47581e6b
ntban15 <ngtab.an.9@gmail.com>
2019-07-02 00:18:51 +0700

    Update evaluation

--- src/main/java/testprioritization/core/demo/EvaluationDemo.java
+++ src/main/java/testprioritization/core/demo/EvaluationDemo.java
@@ -25,11 +25,13 @@ public class EvaluationDemo {
 
     private final String scriptsPath;
     private final String reportsPath;
+    private final String testSuitePath;
 
     public EvaluationDemo(String projectBasePath) {
         this.projectBasePath = projectBasePath;
         scriptsPath = pathMapper.getScriptPathFromBasePath(projectBasePath);
         reportsPath = pathMapper.getReportsPathFromBasePath(projectBasePath);
+        testSuitePath = pathMapper.getTestSuiteFromBasePath(projectBasePath);
     }
 
 
@@ -83,22 +85,70 @@ public class EvaluationDemo {
         // .... Perform APFD evaluation for this method
 
         List<ExecutionResult> executionResults = reportsParser.readAllExecutionResultsFromReportsAt(reportsPath);
-        TestSuite lastExecutedSuiteFromScript = scriptParser.readTestSuiteFromInputAt(scriptsPath);
+        TestSuite lastExecutedSuiteFromScript = scriptParser.readTestSuiteFromInputAt(testSuitePath);
 
         for (RankingAlgorithm rankingAlgorithm : rankingAlgorithms) {
             rankHistoricalExecutions(rankingAlgorithm, executionResults);
+            List<TestCase> lastOrder = rankingAlgorithm.rankTestCasesIn(lastExecutedSuiteFromScript);
+            ExecutionResult lastResult = executionResults.get(executionResults.size() - 1);
+            float apfd = calculateAPFD(lastOrder, lastResult);
+            System.out.println(rankingAlgorithm.getClass().toString() + ": " + apfd);
         }
     }
 
     private List<List<TestCase>> rankHistoricalExecutions(RankingAlgorithm rankingAlgorithm, List<ExecutionResult> executionResults) {
         List<List<TestCase>> historicalOrders = new ArrayList<>();
-        for (ExecutionResult result : executionResults) {
+        for (ExecutionResult result : executionResults) { // n - 1 ??
             List<TestCase> order = rankingAlgorithm.rankTestCasesIn(result.getSuiteUnderExecution());
             rankingAlgorithm.onTestExecutionResult(result);
             historicalOrders.add(order);
         }
 
         return historicalOrders;
+    }
+
+    private List<Float> calculatePercentageOfFaultsFoundPerTestCase(List<TestCase> order, ExecutionResult result) {
+        float numberOfFails = result.getNumberOfFailedTests();
+        float numberOfFailsFound = 0;
+        int numberOfTestCases = order.size();
+        List<Float> results = new ArrayList<>();
+
+        if (numberOfFails == 0) {
+            for (int index = 0; index < numberOfTestCases; index++) {
+                results.add(100f);
+            }
+        }
+
+        for (int index = 0; index < numberOfTestCases; index++) {
+            String testId = order.get(index).getId();
+            if (result.getDidFailForTestCaseWithId(testId)) {
+                numberOfFailsFound += 1;
+            }
+
+            results.add(numberOfFailsFound / numberOfFails * 100f);
+        }
+
+        return results;
+    }
+
+    public Float calculateAPFD(List<TestCase> order, ExecutionResult result) {
+        float numberOfFails = result.getNumberOfFailedTests();
+        float tfSum = 0;
+        int numberOfTestCases = order.size();
+
+        if (numberOfFails == 0) {
+            return 100f;
+        }
+
+        for (int index = 0; index < numberOfTestCases; index++) {
+            String testId = order.get(index).getId();
+            if (result.getDidFailForTestCaseWithId(testId)) {
+                tfSum += index + 1;
+            }
+        }
+
+        float nm = numberOfFails * numberOfTestCases;
+        return (1 - tfSum / nm + 1 / (2 * numberOfTestCases)) * 100;
     }
 
     private class DemoGraphPersistence implements CommandGraphPersistence {
