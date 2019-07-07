2335081ad2fad7e5d629ef9502e5baaae9b99d79
An Nguyen <ngtab.an.9@gmail.com>
2019-07-07 11:18:50 +0700

    Update KatalonReportsParser.java

--- src/main/java/testprioritization/core/katalonstudio/KatalonReportsParser.java
+++ src/main/java/testprioritization/core/katalonstudio/KatalonReportsParser.java
@@ -7,7 +7,6 @@ import testprioritization.core.TestSuite;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
